fde2ac6f0e80529100f3ffbd33807c54064f5796
Martin Dennhardt <martin.dennhardt.ext@siemens-healthineers.com>
2021-03-04 09:32:31 +0100

    Add aliases for backward compatibility

--- module/dotenv/dotenv.psm1
+++ module/dotenv/dotenv.psm1
@@ -1,2 +1,5 @@
 Get-ChildItem $PSScriptRoot\functions -Filter '*.ps1' -Recurse |
-    Foreach-Object { . $_.FullName }
+    ForEach-Object { . $_.FullName }
+
+New-Alias -Name 'Set-DotEnv' -Value 'Push-DotEnv'
+New-Alias -Name 'Remove-DotEnv' -Value 'Pop-DotEnv'
